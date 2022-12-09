8cc785d3df233ce9a06caae3943a6e03ef1b3edd
ted Liefeld <liefeld@broadinstitute.org>
2022-12-09 06:33:22 -0800

    Updating tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # copyright 2017-2018 Regents of the University of California and the Broad Institute. All rights reserved.
-FROM  genepattern/docker-staraligner:0.45
+FROM  genepattern/docker-staraligner:v0.45
 
 COPY star_aligner_wrapper.pl /usr/local/bin/star_aligner_wrapper.pl
 COPY ./* /build/
--- tests/runConfig.sh
+++ tests/runConfig.sh
@@ -1,7 +1,7 @@
 
 INPUT_FILE_DIRECTORIES=$PWD/data
 TASKLIB=$PWD/src/
-COMMAND_LINE="perl $TASKLIB/star_aligner_wrapper.pl --patchesdir / --index $INPUT_FILE_DIRECTORIES/reads12index.zip --reads1 $INPUT_FILE_DIRECTORIES/reads.pair.1.list.txt --reads2 $INPUT_FILE_DIRECTORIES/reads.pair.2.list.txt --alignendtoend no --maxNmismatch 10 --maxFmismatch 0.3 --minoverhangannot 3 --minoverhangnotannot 5 --minintronlength 21 --maxintronlength 500000 --matesmaxgap 500000 --multimapmismatchrange 0 --maxmultimap 10 --canonicaloverhang 12 --noncanonicaloverhang 30 --maponlyreportedjunctions no --twopass no --detectchimeric no --outputunmapped no --quantify no --wiggle None --wigglesignal all --format SAM --HIflag OneBestScore --outputprefix STAR"
+COMMAND_LINE="perl /usr/local/bin/star_aligner_wrapper.pl --index $INPUT_FILE_DIRECTORIES/reads12index.zip --reads1 $INPUT_FILE_DIRECTORIES/reads.pair.1.list.txt --reads2 $INPUT_FILE_DIRECTORIES/reads.pair.2.list.txt --alignendtoend no --maxNmismatch 10 --maxFmismatch 0.3 --minoverhangannot 3 --minoverhangnotannot 5 --minintronlength 21 --maxintronlength 500000 --matesmaxgap 500000 --multimapmismatchrange 0 --maxmultimap 10 --canonicaloverhang 12 --noncanonicaloverhang 30 --maponlyreportedjunctions no --twopass no --detectchimeric no --outputunmapped no --quantify no --wiggle None --wigglesignal all --format SAM --HIflag OneBestScore --outputprefix STAR"
 
 CONTAINER_OVERRIDE_MEMORY=3100
 JOB_DEFINITION_NAME="STAR"
@@ -10,6 +10,6 @@ JOB_QUEUE=TedTest
 S3_ROOT=s3://moduleiotest
 WORKING_DIR=$PWD/job_52345
 
-DOCKER_CONTAINER=genepattern/docker-staraligner
+DOCKER_CONTAINER=genepattern/star.aligner:v0.45
 
 
